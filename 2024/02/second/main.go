4007dfd86272eab0315829eb2b670fd06973d720
cerdelen <cedric.erdelen@gmail.com>
2024-12-02 13:59:08 +0100

    second day: second part

--- 2024/02/second/main.go
+++ 2024/02/second/main.go
@@ -11,6 +11,30 @@ import (
     "strconv"
 )
 
+func isSafe(intArray []int) bool {
+    if intArray[0] < intArray[1] { // we are ascending
+        for i := 1; i < len(intArray); i++ {
+            if !(intArray[i] > intArray[i - 1]) {
+                return false
+            }
+            if !(intArray[i] - intArray[i - 1] < 4) {
+                return false
+            }
+
+        }
+    } else { // we are descending
+        for i := 1; i < len(intArray); i++ {
+            if !(intArray[i] < intArray[i - 1]) {
+                return false
+            }
+            if !(intArray[i - 1] - intArray[i] < 4) {
+                return false
+            }
+        }
+    }
+    return true
+}
+
 func main() {
     file, err := os.Open("input.txt")
     if err != nil {
@@ -34,97 +58,26 @@ func main() {
             intArray[i] = num
         }
 
-        var isSafe bool = true
-        var secondtry bool = false
-        if intArray[0] < intArray[1] { // we are ascending
-            for i := 1; i < len(intArray); i++ {
-                if !(intArray[i] > intArray[i - 1]) {
-                    if !secondtry {
-                        if i == len(intArray - 1) { // if last just remove last
-                            break
-                        }
-                        // idea:
-                        //      i have to check wether i - 1 is outlier or i is outlier
-                        //      i could check wether i is also < i -2
-                        //      basically check wether if i + i - 2 would result into both correct (ascending and < 4)
-                        //      basically check wether if i-1 + i + 1  would result into both correct (ascending and < 4)
-                        // if one of them is correct use that one
-                    } else {
-                        isSafe = false
-                        break
-                    }
-                }
-                if !(intArray[i] - intArray[i - 1] < 4) {
-                    if !secondtry {
-                        intArray = append(intArray[:i], intArray[i+1:]...)
-                        i--
-                        continue
-                    } else {
-                        isSafe = false
-                        break
-                    }
-                }
+        var row_safe bool = isSafe(intArray)
 
-            }
-        } else { // we are descending
-            for i := 1; i < len(intArray); i++ {
-                if !(intArray[i] < intArray[i - 1]) {
-                    if !secondtry {
-                        if i == len(intArray - 1) { // if last just remove last
-                            break
-                        }
-                        // here the other check still
-                    } else {
-                        isSafe = false
-                        break
-                    }
+        if row_safe == false {
+            fmt.Println("From: ", intArray)
+            for i := 0; i < len(intArray); i++ {
+                newArray := make([]int, 0, len(intArray)-1)
+                newArray = append(newArray, intArray[:i]...)
+                newArray = append(newArray, intArray[i+1:]...)
+                fmt.Println("To: ", newArray)
 
-                }
-                if !(intArray[i - 1] - intArray[i] < 4) {
-                    if !secondtry {
-                        intArray = append(intArray[:i], intArray[i+1:]...)
-                        i--
-                        continue
-                    } else {
-                        isSafe = false
-                        break
-                    }
+                row_safe = isSafe(newArray)
+                if row_safe {
+                    break
                 }
             }
+            fmt.Println("")
         }
 
-        // if intArray[0] < intArray[1] { // we are ascending
-        //     for i := 1; i < len(intArray); i++ {
-        //         if !(intArray[i] > intArray[i - 1]) {
-        //             isSafe = false
-        //             fmt.Println("ascending Unsafe because of switch ", intArray)
-        //             break
-        //         }
-        //         if !(intArray[i] - intArray[i - 1] < 4) {
-        //             isSafe = false
-        //             fmt.Println("ascending Unsafe because of >4 ", intArray)
-        //             break
-        //         }
-        //
-        //     }
-        // } else { // we are descending
-        //     for i := 1; i < len(intArray); i++ {
-        //         if !(intArray[i] < intArray[i - 1]) {
-        //
-        //             fmt.Println("descending Unsafe because of switch ", intArray)
-        //             isSafe = false
-        //             break
-        //         }
-        //         if !(intArray[i - 1] - intArray[i] < 4) {
-        //             fmt.Println("descending Unsafe because of > 4", intArray)
-        //             isSafe = false
-        //             break
-        //         }
-        //     }
-        // }
-
-        if isSafe {
-            fmt.Println("Safe: ", intArray)
+        if row_safe {
+            // fmt.Println("Safe: ", intArray)
             safe_reports++
         } else {
             // fmt.Println("Unsafe: ", intArray)
